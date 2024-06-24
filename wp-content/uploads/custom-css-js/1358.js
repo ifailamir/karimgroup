2882eabd3f46c49f77dbf94459cbf2395b11f218
ifailamir <failamir@students.amikom.ac.id>
2024-06-25 00:07:31 +0700

    first

---
+++ wp-content/uploads/custom-css-js/1358.js
@@ -0,0 +1,10 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+  document.addEventListener('DOMContentLoaded', function() {
+    var audio = document.querySelector('audio');
+    if(audio) {
+      audio.play();
+    }
+  });
+</script>
+<!-- end Simple Custom CSS and JS -->
